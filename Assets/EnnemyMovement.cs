6e627088a021b96b5b2c4bca9072400b1fcbd0a3
Florian <florian@deleau.org>
2025-05-19 16:56:41 +0200

    Update background

---
+++ Assets/EnnemyMovement.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+ 
+public class enemymouvement : MonoBehaviour
+{
+    public float speed;
+    public Transform[] waypoints;
+ 
+    private Transform target;
+    private int destPoint = 0;
+ 
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        target = waypoints[0];
+    }
+ 
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 dir = target.position - transform.position;
+        transform.Translate(dir.normalized * speed * Time.deltaTime, Space.World);
+ 
+        if (Vector3.Distance(transform.position, target.position) < 0.3f)
+        {
+            destPoint = (destPoint + 1) % waypoints.Length;
+            target = waypoints[destPoint];
+        }
+    }
+}
